1c502724f684c6402f78e72228e5cab368de387d
abudu <abudulan23@yahoo.com>
2025-01-15 12:20:53 +0200

    Initial commit

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02v2/PracticalTest02v2MainActivity.kt
@@ -0,0 +1,13 @@
+package ro.pub.cs.systems.eim.practicaltest02v2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practical_test02v2_main)
+
+
+    }
+}
